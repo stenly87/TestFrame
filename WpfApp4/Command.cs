015f22c2d25701df08ebc430c819dd4d45eb0d13
Алексей <stenly87@live.ru>
2020-11-17 14:06:48 +1000

    Добавьте файлы проекта.

---
+++ WpfApp4/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp4
+{
+    public class Command : ICommand
+    {
+        Action action;
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action action) => this.action = action;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action?.Invoke();
+        }
+    }
+}
---
+++ WpfApp4/ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp4
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Page currentPage;
+
+        public Command FirstPage { get; set; }
+        public Command SecondPage { get; set; }
+        public Command ThirdPage { get; set; }
+
+        public Page CurrentPage { get => currentPage; set { currentPage = value; RaiseEvent(); } }
+
+        public ViewModel()
+        {
+            FirstPage = new Command(() =>
+            {
+                CurrentPage = new Page1();
+            });
+            SecondPage = new Command(() =>
+            {
+                CurrentPage = new Page2();
+            });
+            ThirdPage = new Command(() =>
+            {
+                CurrentPage = new Page3();
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaiseEvent([CallerMemberName] string prop = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
